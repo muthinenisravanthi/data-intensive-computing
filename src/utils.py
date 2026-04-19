ec2c7ad6af8089bacfa4189409f1decc8c09b51a
dylan <e12332267@student.tuwien.ac.at>
2026-04-19 17:03:27 +0200

    rewrote utils to split on given delimiters

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,11 @@
 import re
 
 def tokenize(text, stopwords):
-    TOKEN_RE = re.compile(r"[^a-zA-Z<>^|]+")
-    text = text.lower()
-    parts = TOKEN_RE.split(text)
+    parts = text.lower()
+    result = re.split(r'[ \s\t\d\(\)\[\]\{\}\.\!\?\,\;\+\=\-\_\"\'\~\#\@\&\*\%\€\$\ß\\\/ ]', parts) #Define the given list of tokens
     
-    tokens = [token for token in parts if token and len(token) > 1 and token not in stopwords]
+    tokens = [token for token in result if token and len(token) > 1 and token not in stopwords]
     return tokens
+
+
+    
