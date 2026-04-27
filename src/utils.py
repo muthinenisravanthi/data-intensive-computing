cb2d173d2643f6ccb1047962787c611b7b759eee
dylan <e12332267@student.tuwien.ac.at>
2026-04-27 20:36:59 +0200

    Added code comments

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,11 @@
 import re
 
-def tokenize(text, stopwords):
-    parts = text.lower()
+def tokenize(text, stopwords):                                                                      
+    parts = text.lower()                                                                            #Case folding
     result = re.split(r'[ \s\t\d\(\)\[\]\{\}\.\!\?\,\;\+\=\-\_\"\'\~\#\@\&\*\%\€\$\ß\\\/ ]', parts) #Define the given list of tokens
     
-    tokens = [token for token in result if token and len(token) > 1 and token not in stopwords]
-    return tokens
+    tokens = [token for token in result if token and len(token) > 1 and token not in stopwords]    #To filter out the stopwords and all the 1 character tokens
+    return tokens                                                                                   #Returns the filtered, tokenized list
 
 
     
